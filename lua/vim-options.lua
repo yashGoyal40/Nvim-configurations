3f04de6ac10e7f880e546eada2cdf8a601d13019
yashGoyal40 <yashg4118@gmail.com>
2024-03-24 21:59:02 +0530

    shift tab

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -13,6 +13,10 @@ vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
 vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
+
+vim.keymap.set('n', '<leader><Tab>', ':split<CR>')
+vim.keymap.set('n', '<leader><Tab><Tab>', ':vsplit<CR>')
+
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
 vim.keymap.set('n','<C-s>',':w!<CR>')
 vim.wo.number = true
