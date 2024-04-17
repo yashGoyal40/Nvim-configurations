a7cca519f6b29692d121a4540bb666e04d5ff2ff
yashGoyal40 <yashg4118@gmail.com>
2024-04-17 21:41:54 +0530

    liveserver

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,8 +2,8 @@ return {
   "nvim-lualine/lualine.nvim",
   config = function()
     require("lualine").setup({
-      options = {
-        theme = "tokyonight",
+      options = { 
+        colorscheme = "gruvbox",
         sections = {
           lualine_a = {
             file = 1,
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -2,6 +2,7 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+
 vim.g.mapleader = " "
 vim.g.background = "light"
 
@@ -19,5 +20,6 @@ vim.keymap.set('n', '<leader><Tab><Tab>', ':vsplit<CR>')
 
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
 vim.keymap.set('n','<C-s>',':w!<CR>')
+vim.keymap.set('n','<C-i>',':LiveServerStart<CR>')
 vim.wo.number = true
 
